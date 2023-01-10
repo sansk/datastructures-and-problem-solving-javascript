278cd9c30ef891d672bb20bd927deff0b4bf0fae
sansk <iamsangyk@gmail.com>
2023-01-10 14:31:23 +0530

    changes

--- 01-Basic/ProperDivisors/index.js
+++ 01-Basic/ProperDivisors/index.js
@@ -1,9 +1,9 @@
 const properDivisors = n => {
     let divisors = [1];
-    
+
     // Condition can either be `i <= sqrt(n)` OR `i * i <= n`. Both are equivalent.
     // if square of `i` = `n`, then always square root of `n` = `i`
-    for (let i = 2; i * i <= n; i++) { 
+    for (let i = 2; i * i <= n; i++) {
         divisors.pop();
         if (n % i === 0) {
             divisors.push(i, n / i);
