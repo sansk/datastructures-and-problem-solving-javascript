fdb2e1975800c5ca27252e27739454e4f00cc8f4
sansk <iamsangyk@gmail.com>
2023-01-11 06:31:20 +0530

    Updated

--- 01-Basic/ProperDivisors/index.js
+++ 01-Basic/ProperDivisors/index.js
@@ -4,7 +4,6 @@ const properDivisors = n => {
     // Condition can either be `i <= sqrt(n)` OR `i * i <= n`. Both are equivalent.
     // if square of `i` = `n`, then always square root of `n` = `i`
     for (let i = 2; i * i <= n; i++) {
-        divisors.pop();
         if (n % i === 0) {
             divisors.push(i, n / i);
             if (i === Math.sqrt(n)) { // Condition can either be `i === sqrt(n)` OR `i * i === n`. Both are equivalent.
@@ -15,6 +14,4 @@ const properDivisors = n => {
     return divisors.sort(function (a, b) { return a - b; });
 }
 
-console.time("Time-Taken:");
 console.log(properDivisors(144));
-console.timeEnd("Time-Taken:");
